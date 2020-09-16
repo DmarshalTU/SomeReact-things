237fb33d52e59ee4c745bc5b0fc8d805f28bc218
Denis Tumakov <dmarshaltu@gmail.com>
2020-09-16 10:28:01 -0700

    Add files via upload

---
+++ Rock,Paper,Scissors/precache-manifest.61e0c38b8685e3c009f01456d7adfe67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7618a2ea9423bf1e87cccb084e1b86b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "947b3440332617563bad",
+    "url": "/static/css/main.7531e9c8.chunk.css"
+  },
+  {
+    "revision": "47624357071bc069735e",
+    "url": "/static/js/2.64cf4f26.chunk.js"
+  },
+  {
+    "revision": "b70864f2f03b728e9eec72cc85f6eb48",
+    "url": "/static/js/2.64cf4f26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "947b3440332617563bad",
+    "url": "/static/js/main.55422189.chunk.js"
+  },
+  {
+    "revision": "6e198a336cdf105e14f4",
+    "url": "/static/js/runtime-main.be4df282.js"
+  }
+]);
