aae6514506a4e948cca303d50684254b20419ee7
Denis Tumakov <dmarshaltu@gmail.com>
2020-09-16 10:31:15 -0700

    Add files via upload

---
+++ AI-face-App-master/README.md
@@ -0,0 +1,4 @@
+# AI-face-App
+Render random AI Person face App
+
+https://relaxed-shannon-de1e99.netlify.app/
---
+++ AI-face-App-master/build/precache-manifest.3307dd16dbb19c2d021d8cbb4dfd1494.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a6c5118d39625a6f12de3281fe8a01d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fb32cf59387dff3169a",
+    "url": "/static/css/main.32182184.chunk.css"
+  },
+  {
+    "revision": "87ad2718a9d0e8d10e11",
+    "url": "/static/js/2.0726cafd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0726cafd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fb32cf59387dff3169a",
+    "url": "/static/js/main.cc16eb0b.chunk.js"
+  },
+  {
+    "revision": "04e8eed1a43108c22fb7",
+    "url": "/static/js/runtime-main.9173d584.js"
+  }
+]);
